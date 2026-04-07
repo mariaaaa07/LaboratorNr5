996d44b9f7a4d117d8947fa754994e2ea9d3cef9
mariaaaa07 <gherasim.maria30@gmail.com>
2026-04-07 07:36:50 +0300

    Update Ex4.c

--- Ex4.c
+++ Ex4.c
@@ -4,7 +4,6 @@
 
 #define MAX 100
 
-// Structura pentru Coadă (folosită pentru joc)
 int coada[MAX];
 int front = 0, rear = -1, nrElemCoadă = 0;
 
@@ -21,7 +20,6 @@ int dequeue() {
     return x;
 }
 
-// Structura pentru Stivă (folosită pentru salvarea ordinii inverse)
 int stiva[MAX];
 int top = -1;
 
@@ -35,37 +33,30 @@ int pop() {
 
 int main() {
     int x, i, j;
-    srand(time(NULL)); // Inițializăm generatorul de numere random
+    srand(time(NULL)); 
 
     printf("Introduceți numărul de jucători: ");
     scanf("%d", &x);
 
-    // 1. Punem toți jucătorii în coadă (jucătorul 1, 2, ..., x)
     for (i = 1; i <= x; i++) {
         enqueue(i);
     }
 
     printf("\nÎncepe jocul!\n");
 
-    // Jocul continuă până când rămâne un singur jucător
     while (nrElemCoadă > 0) {
-        // Generăm un număr random de "pasări" ale cartofului (între 1 și 10)
         int hotPotato = (rand() % 10) + 1;
         
-        // Mutăm jucătorii de la început la final de 'hotPotato - 1' ori
         for (j = 0; j < hotPotato - 1; j++) {
             enqueue(dequeue());
         }
 
-        // Jucătorul care a rămas cu cartoful (al 'hotPotato'-lea) este eliminat
         int eliminat = dequeue();
         printf("Jucătorul %d a prins cartoful fierbinte și a ieșit!\n", eliminat);
         
-        // Îl salvăm în stivă pentru afișarea finală
         push(eliminat);
     }
 
-    // 2. Afișăm rezultatele în ordine inversă (folosind stiva)
     printf("\nClasament (de la ultimul rămas la primul ieșit):\n");
     while (top != -1) {
         printf("%d ", pop());
